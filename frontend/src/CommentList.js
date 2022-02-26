4fcdcec14c8aa561339f6100224a6a653d0581b9
mriduld <mridul.dey@ellucian.com>
2022-02-26 05:43:29 +0530

    comment moderation notification added in frontend

--- frontend/src/CommentList.js
+++ frontend/src/CommentList.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
-  const renderComments = comments.map(({ id, content }) => {
-    return <li key={id}>{content}</li>;
+  const renderComments = comments.map(({ id, content, status }) => {
+    let commentContent = "";
+
+    if (status === "approved") {
+      commentContent = content;
+    }
+
+    if (status === "pending") {
+      commentContent = "This comment is awaiting for moderation";
+    }
+
+    if (status === "rejected") {
+      commentContent = "This comment is rejected";
+    }
+
+    return commentContent && <li key={id}>{commentContent}</li>;
   });
   return <ul>{renderComments}</ul>;
 };
